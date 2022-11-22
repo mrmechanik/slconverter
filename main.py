9d0f54bff1b9b0d19a3aed68796400fb75f75611
Lukas Ganzke <lukas.ganzke@hochschule-stralsund.de>
2022-11-22 17:56:33 +0100

    refactored framegroup methods
    - made framegrop chainable
    - added more type hints
    - added decorators for logging and creating a new instance
    - made extracted frames comparable & hashable
    - removed unused properties
    - made more method private

--- main.py
+++ main.py
@@ -14,36 +14,33 @@ logging.getLogger('matplotlib').setLevel(logging.INFO)
 
 
 def test():
-    frame_dict = load_files('data')
+    frame_dict = load_files('data/')
 
     frames: [ExtractedFrame] = []
 
     for f in frame_dict.values():
         frames.extend(f)
 
-    h = FrameGroup(frames)
-    xs = h.x_vector
-    ys = h.y_vector
-    zs = h.z_vector
+    h: FrameGroup = FrameGroup(frames).filter().uniquify().normalize()
 
-    dim = (2, 2)
+    dim: tuple[int, int] = (2, 2)
     fig: Figure = plt.figure(figsize=(20, 20))
 
     logger.debug('Generating Plot 1')
     ax1: Axes = fig.add_subplot(*dim, 1, title='Depth Distribution')
-    sns.kdeplot(zs, ax=ax1)
+    sns.kdeplot(h.zs, ax=ax1)
 
     logger.debug('Generating Plot 2')
     ax2: Axes = fig.add_subplot(*dim, 2, title='Water Body Coverage')
-    ax2.plot(xs, ys)
+    ax2.plot(h.xs, h.ys)
 
     logger.debug('Generating Plot 3')
     ax3: Axes3D = fig.add_subplot(*dim, 3, title='3D Visualization', projection='3d')
-    ax3.plot_trisurf(xs, ys, list(map(lambda v: 0 - v, zs)), cmap='jet', edgecolor='none')
+    ax3.plot_trisurf(h.xs, h.ys, list(map(lambda v: 0 - v, h.zs)), cmap='jet', edgecolor='none')
 
     logger.debug('Generating Plot 4')
     ax4: Axes = fig.add_subplot(*dim, 4, title='Bathymetric Map')
-    ax4.tricontourf(xs, ys, zs, cmap='jet')
+    ax4.tricontourf(h.xs, h.ys, h.zs, cmap='jet')
 
     plt.show()
 
--- models.py
+++ models.py
@@ -1,145 +1,171 @@
 import logging
+import time
+from functools import wraps
 
 from sllib import Frame
 from sllib.definitions import FEET_CONVERSION
 
 logger = logging.getLogger(__name__)
 default_sensitivity = 0.07
+auto = 'auto'
+
+
+def proc_time_log(msg):
+    def deco(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            logger.debug(msg)
+            timer: float = time.time()
+            res = func(*args, **kwargs)
+            logger.debug(f'Took {time.time() - timer} s')
+            return res
+
+        return wrapper
+
+    return deco
+
+
+def update_and_return_group(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs) -> 'FrameGroup':
+        res: [ExtractedFrame] = func(self, *args, **kwargs)
+        return FrameGroup(res)
+
+    return wrapper
 
 
 class ExtractedFrame:
     def __init__(self, frame: Frame):
         self.keel_depth_m: float = frame.keel_depth * FEET_CONVERSION if hasattr(frame, 'keel_depth') else 0
         self.actual_water_depth_m: float = frame.water_depth_m + self.keel_depth_m
-        self.latitude = frame.latitude
-        self.longitude = frame.longitude
+        self.latitude: float = frame.latitude
+        self.longitude: float = frame.longitude
 
-    def __repr__(self):
-        return '{:.2f}'.format(self.actual_water_depth_m)
+    def __as_tuple(self) -> tuple[float, float, float, float]:
+        return self.keel_depth_m, self.actual_water_depth_m, self.latitude, self.longitude
+
+    def __eq__(self, other) -> bool:
+        return isinstance(other, self.__class__) and self.__as_tuple() == other.__as_tuple()
+
+    def __hash__(self) -> int:
+        return hash(self.__as_tuple())
+
+    def __repr__(self) -> str:
+        return '{{{:.6f} {:.6f} {:.2f}}}'.format(self.latitude, self.longitude, self.actual_water_depth_m)
 
 
 class FrameGroup:
-    def __init__(
-            self, frames: [ExtractedFrame],
-            use_highest_keel_depth_m_as_min_depth: bool = True,
-            use_reasonable_depth_as_max_depth: bool | float = True
-    ):
-        self._low: float = 0
-        self._high: float = 0
+    __xs: [float] = []
+    __ys: [float] = []
+    __zs: [float] = []
 
+    def __init__(self, frames: [ExtractedFrame]):
         self._frames: [ExtractedFrame] = frames
-
-        self._xs: [float] = []
-        self._ys: [float] = []
-        self._zs: [float] = []
-
-        if use_highest_keel_depth_m_as_min_depth:
-            self._low = self.max_keel_m
-            logger.debug(f'Detected {self._low} as lower border to invalid measurements')
-
-        if use_reasonable_depth_as_max_depth:
-            if type(use_reasonable_depth_as_max_depth) == bool:
-                sensitivity = default_sensitivity
-            else:
-                sensitivity = use_reasonable_depth_as_max_depth
-
-            logger.debug('{} as maximum difference'.format(f'Default of {default_sensitivity}' if type(
-                use_reasonable_depth_as_max_depth
-            ) == bool else use_reasonable_depth_as_max_depth))
-
-            self._high = self.get_max_reasonable_depth(sensitivity)
-            logger.debug(f'Detected {self._high} as upper border value to invalid measurements')
+        self._hash = hash(tuple(frames))
 
     @property
     def frames(self) -> [ExtractedFrame]:
+        self.__validate_existence()
         return self._frames.copy()
 
     @property
-    def min_depth(self) -> float:
-        return self._low
-
-    @min_depth.setter
-    def min_depth(self, value: float):
-        self._validate_depth(value)
-        self._low = value
+    def xs(self) -> list[float]:
+        self.__validate_existence()
+        return self.__xs.copy()
 
     @property
-    def max_depth(self) -> float:
-        return self._high
-
-    @max_depth.setter
-    def max_depth(self, value: float):
-        self._validate_depth(value)
-        self._high = value
+    def ys(self) -> list[float]:
+        self.__validate_existence()
+        return self.__ys.copy()
 
     @property
-    def limited_frames(self):
-        return self._limited_frames().copy()
+    def zs(self) -> list[float]:
+        self.__validate_existence()
+        return self.__zs.copy()
 
-    @property
-    def x_vector(self) -> list[float]:
-        self._build_vectors()
-        return self._xs.copy()
+    def get_max_keel_m(self) -> float:
+        return max(map(lambda f: f.keel_depth_m, self._frames))
 
-    @property
-    def x_set(self):
-        return list({*self.x_vector})
+    @proc_time_log('Calculating maximum plausible depth...')
+    def get_max_plausible_depth(self, low: float, sensitivity: float) -> float:
+        depths: [float] = sorted(set(filter(lambda m: m > low, map(lambda f: f.actual_water_depth_m, self._frames))))
+        max_depth = next((cur for cur, fut in zip(depths, depths[1:]) if fut - cur >= sensitivity), depths[-1])
+        return max_depth
 
-    @property
-    def y_vector(self) -> list[float]:
-        self._build_vectors()
-        return self._ys.copy()
+    def filter_min(self, min_border: float | str = auto) -> [ExtractedFrame]:
+        return self.filter(min_border, 20000)
 
-    @property
-    def y_set(self):
-        return list({*self.y_vector})
+    def filter_max(self, max_border: float | str = auto, sensitivity: float = default_sensitivity) -> [ExtractedFrame]:
+        return self.filter(0, max_border, sensitivity)
 
-    @property
-    def z_vector(self) -> list[float]:
-        self._build_vectors()
-        return self._zs.copy()
+    @proc_time_log('Filtering frames...')
+    @update_and_return_group
+    def filter(
+            self, min_border: float | str = auto, max_border: float | str = auto,
+            sensitivity_for_max: float = default_sensitivity
+    ) -> [ExtractedFrame]:
+        self.__validate_param(min_border)
+        self.__validate_param(max_border)
 
-    @property
-    def z_set(self):
-        return list({*self.z_vector})
+        low = self.get_max_keel_m() if min_border == auto else min_border
+        high = self.get_max_plausible_depth(low, sensitivity_for_max) if max_border == auto else max_border
 
-    @property
-    def max_keel_m(self) -> float:
-        return max(map(lambda f: f.keel_depth_m, self.frames))
+        if low >= high:
+            raise ValueError(f'No outliers detectable for LOW {low} and HIGH {high}')
 
-    def get_max_reasonable_depth(self, sensitivity: float) -> float:
-        ordered_depths: [float] = sorted(
-            set(filter(lambda m: m > self._low, map(lambda f: f.actual_water_depth_m, self.frames)))
-        )
+        return list(filter(lambda f: low < f.actual_water_depth_m <= high, self._frames))
 
-        return next(
-            (cur_d for cur_d, next_d in zip(ordered_depths, ordered_depths[1:]) if next_d - cur_d >= sensitivity),
-            ordered_depths[-1]
-        )
+    @proc_time_log('Removing duplicates...')
+    @update_and_return_group
+    def uniquify(self) -> [ExtractedFrame]:
+        uniques: set = set()
+        uniques_add = uniques.add
+        ordered_uniques: [float] = [f for f in self._frames if not (f in uniques or uniques_add(f))]
+        logger.debug(f'Reduced {len(self._frames)} frames to {len(ordered_uniques)} frames')
+        return ordered_uniques
 
-    def clear(self):
-        self._xs.clear()
-        self._ys.clear()
-        self._zs.clear()
+    @update_and_return_group
+    def normalize(self, min_x: float | str = auto, min_y: float | str = auto) -> [ExtractedFrame]:
+        self.__validate_param(min_x)
+        self.__validate_param(min_y)
+
+        if min_x == auto:
+            min_x = min(map(lambda f: f.latitude, self._frames))
+
+        if min_y == auto:
+            min_y = min(map(lambda f: f.longitude, self._frames))
+
+        return list(map(lambda f: self.__normalize_row(min_x, min_y, f), self._frames))
 
     @staticmethod
-    def _validate_depth(value: float):
-        if value < 0:
-            raise ValueError('Depth cannot be negative')
+    def __normalize_row(min_x: float, min_y: float, frame: ExtractedFrame) -> ExtractedFrame:
+        frame.latitude = frame.latitude - min_x
+        frame.longitude = frame.longitude - min_y
+        return frame
 
-    def _limited_frames(self) -> [ExtractedFrame]:
-        if self._low >= self._high:
-            return self.frames
+    @staticmethod
+    def __validate_param(param: float | str) -> None:
+        if type(param) == str and param != auto:
+            raise ValueError(f'Parameters may only be floats if not set to "{auto}"')
 
-        return list(filter(lambda f: self._low < f.actual_water_depth_m <= self._high, self.frames))
+    def __validate_existence(self):
+        cur_hash = hash(tuple(self._frames))
 
-    def _build_vectors(self):
-        if self._xs:
-            return
+        if cur_hash != self._hash or not self.__xs:
+            self.__rebuild_vectors()
+            self._hash = cur_hash
 
-        any(map(self._split_row, self._limited_frames()))
+    @proc_time_log(f'Reconstructing...')
+    def __rebuild_vectors(self) -> None:
+        self.__clear()
+        any(map(self.__split_row, self._frames))
 
-    def _split_row(self, frame: ExtractedFrame):
-        self._xs.append(frame.latitude)
-        self._ys.append(frame.longitude)
-        self._zs.append(frame.actual_water_depth_m)
+    def __clear(self) -> None:
+        self.__xs.clear()
+        self.__ys.clear()
+        self.__zs.clear()
+
+    def __split_row(self, frame: ExtractedFrame) -> None:
+        self.__xs.append(frame.latitude)
+        self.__ys.append(frame.longitude)
+        self.__zs.append(frame.actual_water_depth_m)
